8835d56cec587f9478e4657257d678a277287201
sandrusmb <sandrusmb@gmail.com>
2020-01-22 11:51:24 +0100

    Update docs

--- docs/assets/js/main.js
+++ docs/assets/js/main.js
@@ -1,1 +1,1 @@
-"use strict";const grid=new Muuri(".grid",{layout:{rounding:!1}});window.addEventListener("load",()=>{document.getElementById("grid").classList.add("js-imagenes-cargadas")});
+"use strict";const grid=new Muuri(".grid",{layout:{rounding:!1}});window.addEventListener("load",()=>{grid.refresItems().layout(),document.getElementById("grid").classList.add("imagenes-cargadas")});
