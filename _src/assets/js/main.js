931715d6404dee5387b702d36f13caaf2e5fee49
sandrusmb <sandrusmb@gmail.com>
2020-01-23 10:55:08 +0100

    Add filter by category

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -7,6 +7,22 @@ const grid = new Muuri(".grid", {
 });
 
 window.addEventListener("load", () => {
-  grid.refresItems().layout();
+  grid.refreshItems().layout();
   document.getElementById("grid").classList.add("imagenes-cargadas");
+
+  const enlaces = document.querySelectorAll("#categorias a");
+  console.log(enlaces);
+  enlaces.forEach(elemento => {
+    elemento.addEventListener("click", ev => {
+      ev.preventDefault();
+      enlaces.forEach(enlace => enlace.classList.remove("active"));
+      ev.target.classList.add("active");
+
+      const category = ev.target.innerHTML.toLowerCase();
+      console.log(category);
+      category === "todos"
+        ? grid.filter("[data-category]")
+        : grid.filter(`[data-category=${category}]`);
+    });
+  });
 });
