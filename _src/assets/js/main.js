c8804e7176ec4c88e1dc4b9fb8b201931e2568a4
sandrusmb <sandrusmb@gmail.com>
2020-01-23 17:08:52 +0100

    Add listener to the input text

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -10,8 +10,9 @@ window.addEventListener("load", () => {
   grid.refreshItems().layout();
   document.getElementById("grid").classList.add("imagenes-cargadas");
 
+  //listener de los enlaces para filtrar por categoría
   const enlaces = document.querySelectorAll("#categorias a");
-  console.log(enlaces);
+
   enlaces.forEach(elemento => {
     elemento.addEventListener("click", ev => {
       ev.preventDefault();
@@ -19,10 +20,19 @@ window.addEventListener("load", () => {
       ev.target.classList.add("active");
 
       const category = ev.target.innerHTML.toLowerCase();
-      console.log(category);
+
       category === "todos"
         ? grid.filter("[data-category]")
         : grid.filter(`[data-category=${category}]`);
     });
   });
+
+  // listener para el input de texto
+  document.querySelector(".header__input").addEventListener("keyup", ev => {
+    console.log(ev.target.value);
+    const busqueda = ev.target.value;
+    grid.filter(item => item.getElement().dataset.tags.includes(busqueda));
+  });
 });
+
+//Mejoras: separar las funciones. Añadir un mensaje cuando no encuentre fotos buscando por el input de texto.
