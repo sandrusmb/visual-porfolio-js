8085924f1a76aa59374c2d080aad6738df0c544f
sandrusmb <sandrusmb@gmail.com>
2020-01-24 21:10:46 +0100

    Add images listener and close button listener

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -33,6 +33,28 @@ window.addEventListener("load", () => {
     const busqueda = ev.target.value;
     grid.filter(item => item.getElement().dataset.tags.includes(busqueda));
   });
+
+  //listener para las imágenes
+
+  const overlay = document.querySelector(".overlay");
+  document
+    .querySelectorAll(".grid__item__contenido__foto")
+    .forEach(elemento => {
+      elemento.addEventListener("click", () => {
+        const ruta = elemento.getAttribute("src");
+        const description = elemento.parentNode.parentNode.dataset.description;
+        overlay.classList.add("js-active");
+        document.querySelector(".overlay__container__picture").src = ruta;
+        document.querySelector(".overlay__text").innerHTML = description;
+      });
+    });
+
+  //eventlistener del boton de cerrar
+  document
+    .querySelector(".overlay__container__btn")
+    .addEventListener("click", () => {
+      overlay.classList.remove("js-active");
+    });
 });
 
 //Mejoras: separar las funciones. Añadir un mensaje cuando no encuentre fotos buscando por el input de texto. Coger los datos de un json/api.
